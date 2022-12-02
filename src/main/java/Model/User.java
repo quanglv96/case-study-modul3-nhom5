178096218b086ab301c1681d6d64e730c5f38b7d
viethoang9999 <viethoang9999>
2022-12-02 09:53:31 +0700

    Thêm dữ liệu Class User

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,5 +1,80 @@
 package Model;
 
 public class User {
-    private String abc;
+    private int idUser;
+    private String userName;
+    private String password;
+    private int phoneNumber;
+    private String email;
+    private String address;
+    private int statusUser;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStatusUser() {
+        return statusUser;
+    }
+
+    public void setStatusUser(int statusUser) {
+        this.statusUser = statusUser;
+    }
+
+    public User() {
+    }
+
+    public User(int idUser, String userName, String password, int phoneNumber, String email, String address, int statusUser) {
+        this.idUser = idUser;
+        this.userName = userName;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.statusUser = statusUser;
+    }
 }
